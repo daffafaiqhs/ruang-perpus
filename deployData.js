b443b755a86833e307bffaafd57214e46074d990
daffafaiqhs <daffafaiqhs@gmail.com>
2024-10-13 16:06:33 +0700

    change link

--- deployData.js
+++ deployData.js
@@ -10,6 +10,6 @@ const studentName = 'Mochammad Daffa Faiq Husin Syahputra';
 
 const studentId = 'FS9869007';
 
-const NetlifyDeployUrl = 'https://sparkly-concha-2bac4e.netlify.app/';
+const NetlifyDeployUrl = 'https://clinquant-bienenstitch-20ad5e.netlify.app/';
 
 module.exports = { studentName, studentId, NetlifyDeployUrl };
